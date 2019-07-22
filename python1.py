4210241f4bdd384bcc031e2098c98e2a45d0553d
JWJ <53131121+JWJ988@users.noreply.github.com>
2019-07-22 10:08:31 +0800

    Create a phython file
    Create a file with GitHub for the first time

---
+++ python1.py
@@ -0,0 +1,23 @@
+1.Print打印
+语句末尾不加分号,用于分隔的逗号后要加空格
+#print可以接受多个字符串，用逗号分隔，遇到逗号会输出一个空格
+ print("今天是星期一"，“阴”，“天气闷热”)
+#输出数字
+ print(2019)
+#输出运算结果，运算符两侧习惯加空格
+ print(10+8)
+ print("10+8 =", 18)
+ 
+2.input输入
+#input等待输入（阻塞），在执行窗口键盘输入
+ age=input("输入年龄：")
+ print("age =", age)
+
+3.python的数据类型
+Number、String、Boolean、None、List、Tuple（元祖）、Set、Dict（字典）
+
+4.查看Python关键字
+import keyword
+print（keyword.kwlist）
+
+
